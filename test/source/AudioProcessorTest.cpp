b72dd3f7ea004a7cc302eb439851c574e88792c4
artm1904 <artmsavchenko@yandex.ru>
2025-04-26 17:46:34 +0300

    first commit

---
+++ test/source/AudioProcessorTest.cpp
@@ -0,0 +1,10 @@
+#include <gtest/gtest.h>
+#include "eq_plagin/PluginProcessor.h"
+
+namespace eq_plagin_test {
+TEST(EQ_Plagin, Foo) {
+  AudioPluginAudioProcessor processor();
+  ASSERT_FALSE(true);
+}
+
+}  // namespace eq_plagin_test
