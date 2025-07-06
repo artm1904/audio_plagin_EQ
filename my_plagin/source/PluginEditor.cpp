58f76554a3922c788f9361d1cf840f396f7b0588
artm1904 <artmsavchenko@yandex.ru>
2025-07-06 23:23:45 +0300

    Made a display basic parameters values

--- my_plagin/source/PluginEditor.cpp
+++ my_plagin/source/PluginEditor.cpp
@@ -24,25 +24,39 @@ void LookAndFeel::drawRotarySlider(juce::Graphics &g, int x, int y, int width, i
   g.setColour(Colour(255u, 154u, 1u));
   g.drawEllipse(bounds, 1.f);
 
-  auto center = bounds.getCentre();
+  if (auto *rswl = dynamic_cast<RotarySliderWithLabels *>(&slider)) {
+    auto center = bounds.getCentre();
 
-  Path p;
+    Path p;
 
-  Rectangle<float> r;
-  r.setLeft(center.getX() - 2);
-  r.setRight(center.getX() + 2);
-  r.setTop(bounds.getY());
-  r.setBottom(center.getY());
+    Rectangle<float> r;
+    r.setLeft(center.getX() - 2);
+    r.setRight(center.getX() + 2);
+    r.setTop(bounds.getY());
+    r.setBottom(center.getY() - rswl->getTextHeight() * 1.5);
 
-  p.addRectangle(r);
+    p.addRoundedRectangle(r, 2.f);
+    jassert(rotaryStartAngle < rotaryEndAngle);
 
-  jassert(rotaryStartAngle < rotaryEndAngle);
+    auto sliderAngRad = jmap(sliderPosProportional, 0.f, 1.f, rotaryStartAngle, rotaryEndAngle);
 
-  auto sliderAngRad = jmap(sliderPosProportional, 0.f, 1.f, rotaryStartAngle, rotaryEndAngle);
+    p.applyTransform(AffineTransform().rotated(sliderAngRad, center.getX(), center.getY()));
 
-  p.applyTransform(AffineTransform().rotated(sliderAngRad, center.getX(), center.getY()));
+    g.fillPath(p);
 
-  g.fillPath(p);
+    g.setFont(rswl->getTextHeight());
+    auto text = rswl->getDisplayString();
+    auto strWidth = g.getCurrentFont().getStringWidth(text);
+
+    r.setSize(strWidth + 4, rswl->getTextHeight() + 2);
+    r.setCentre(bounds.getCentre());
+
+    g.setColour(Colours::black);
+    g.fillRect(r);
+
+    g.setColour(Colours::white);
+    g.drawFittedText(text, r.toNearestInt(), juce::Justification::centred, 1);
+  }
 }
 
 void RotarySliderWithLabels::paint(juce::Graphics &g) {
@@ -57,7 +71,7 @@ void RotarySliderWithLabels::paint(juce::Graphics &g) {
 
   g.setColour(Colours::red);
   g.drawRect(getLocalBounds());
-    g.setColour(Colours::yellow);
+  g.setColour(Colours::yellow);
   g.drawRect(sliderBounds);
 
   getLookAndFeel().drawRotarySlider(g, sliderBounds.getX(), sliderBounds.getY(),
@@ -83,6 +97,8 @@ juce::Rectangle<int> RotarySliderWithLabels::getSliderBounds() const {
 
   return r;
 }
+
+juce::String RotarySliderWithLabels::getDisplayString() const { return juce::String(getValue()); }
 
 //==============================================================================
 
